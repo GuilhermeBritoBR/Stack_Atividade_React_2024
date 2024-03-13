0baef06de6a8e50690a107b00ee189d46c340813
Guilherme Santiago de Brito <guilhermesantiagodebritod.s@gmail.com>
2024-03-13 08:51:04 -0300

    done

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -5,3 +5,4 @@ export const Logo = {uri: 'https://cdn.esahubble.org/archives/images/thumb300y/p
 export const BackgroundContact = {uri: 'https://hubblesite.org/files/live/sites/hubble/files/home/mission-and-telescope/hubble-30th-anniversary/iconic-images/_images/hubble_30th_images/hubble-30th-star-cluster-ngc-602.jpg'}
 export const BackgroundSobre = {uri: 'https://hubblesite.org/files/live/sites/hubble/files/home/mission-and-telescope/hubble-30th-anniversary/iconic-images/_images/hubble_30th_images/hubble-30th-veil-nebula.jpg'}
 export const BackgroundLogin = {uri: 'https://images6.alphacoders.com/121/1217194.png'}
+
